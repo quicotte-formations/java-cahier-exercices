53288c41a523bfd6abd93745a71ea6ea525619c1
quicotte-formation <quicotte@gmail.com>
2021-10-04 15:00:02 +0200

    v1.0

---
+++ src/cap/atelier/pointentree/Variables.java
@@ -0,0 +1,14 @@
+package cap.atelier.pointentree;
+
+import java.util.Scanner;
+
+public class Variables {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner( System.in );
+		
+		String res = scanner.next();
+	}
+
+}
