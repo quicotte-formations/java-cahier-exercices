a32285c537dd531527d0581a8323f415eadecfee
quicotte-formation <quicotte@gmail.com>
2021-10-05 09:32:16 +0200

    collections: en cours

---
+++ src/cap/atelier/pointentree/Collections.java
@@ -0,0 +1,38 @@
+package cap.atelier.pointentree;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Collections {
+
+	public static void main(String[] args) {
+
+		// EX 1
+		Scanner scanner = new Scanner( System.in);
+		ArrayList<String> nombres = new ArrayList<>();
+		
+//		String n = scanner.next(); // VERSION LONGUE
+//		nombres.add(n);
+//		n = scanner.next();
+//		nombres.add(n);
+//		n = scanner.next();
+//		nombres.add(n);
+		
+		nombres.add( scanner.next() );// VERSION COURTE
+		nombres.add( scanner.next() );
+		nombres.add( scanner.next() );
+		
+		
+		// EX 2
+//		ArrayList<String> nombres = new ArrayList<>();
+//		
+//		nombres.add("10");
+//		nombres.add("20");
+//		nombres.add(0, "5");
+		
+		
+	}
+
+}
