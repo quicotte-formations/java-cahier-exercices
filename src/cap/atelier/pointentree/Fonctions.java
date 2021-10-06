fb1a416a7f741da941d5ac81c8350ea949bdf74e
quicotte-formation <quicotte@gmail.com>
2021-10-06 11:05:51 +0200

    Fonctions : solutions OK

--- src/cap/atelier/pointentree/Fonctions.java
+++ src/cap/atelier/pointentree/Fonctions.java
@@ -1,34 +1,69 @@
 package cap.atelier.pointentree;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Fonctions {
-
 	
-	public static void afficheNFois(String texte, int n) {
-		for(int i=0;i<n;i++) {
-			System.out.println( texte );
+	public static String majuscules(String ... mesStrings) {
+		
+		String resultat = "";
+		
+		for(String stringAct : mesStrings) {
+			resultat += stringAct + ", ";// res = res + stringAct + ", "
 		}
+		
+		return resultat;
 	}
 	
-	public static String afficheMenuEtRenvoieChoix() {
-		System.out.println("MENU");
-		System.out.println("****");
-		System.out.println("1 : Ajouter");
-		System.out.println("2 : Soustraire");
-		System.out.println("3 : Quitter");
-		System.out.print("Votre choix : ");
-		Scanner s = new Scanner(System.in);
-		String choix = s.next();
+	public static int carre(int n) {
+
+		return puissance( n, 2);
+	}
+	
+	public static int puissance(int n, int p) {
+
+		int res = n;
+		for(int i=0;i<p;i++) {
+			res *= n;			
+		}
 		
-		return choix;
+		return n;
+	}
+	
+	public static int factorielle(int n) {
+		
+		int res = n;
+		for(n=n-1;n>1;n--) {
+			res *= n;// res = res *n: n++, n--
+		}
+		
+		return res;
 	}
 	
 	public static void main(String[] args) {
 		
-		afficheNFois("coucou", 3);
+		// EX 7
+		ArrayList<Integer> entiers = new ArrayList<>();
+		entiers.add(10);
+		entiers.add(20);
+		entiers.add(30);
+		entiers.add(42);
 		
-		afficheNFois("hello", 10);
+//		LAMBDA : NOTATION LONGUE
+//		entiers.forEach(   (entierAct)->{
+//			System.out.println( carre(entierAct) );
+//		}    );
+		
+//		LAMBDA :  NOTATION COURTE
+		entiers.forEach( elemActuel -> System.out.println( carre(elemActuel) ) );
+		
+		// EQUIVALENT FOR
+//		for(Integer elemActuel : entiers) {
+//			System.out.println("APPEL LAMBDA pour : " + elemActuel);
+//		}
+		
+			
 	}
 
 }
